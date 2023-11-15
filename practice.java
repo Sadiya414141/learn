b46df5f6f16e531be157e4b6476e182fd9181375
Sadiya_Bagwan <102041355+Sadiya414141@users.noreply.github.com>
2023-11-15 18:42:13 +0530

    Add files via upload
    using new updated method of switch

---
+++ practice.java
@@ -0,0 +1,18 @@
+
+public class practice{
+    public static void main(String []args){
+        String day = "Monday";
+        switch (day)
+    {
+        case "satuday","Sunday"  -> System.out.println("8 am");
+            
+    
+        case "Monday" -> System.out.println("6 am");
+          
+        default   -> System.out.println("7 am");
+        
+        
+    }
+    }
+    
+}
